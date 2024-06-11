565edae9744a95ef6a527d69364af2ea2342179c
Divyesh Reddy.E <divyeshreddy2003@gmail.com>
2024-06-11 11:21:06 +0530

    Name and Icon Change

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
-import Contact from "./components/Contact"
+import Contact from "./components/Contact";
+import { Helmet } from "react-helmet";
 
 
 function App() {
   return (
    <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Divyesh Reddy </title>
+      </Helmet>
       <NavBar /> 
       <Home />
       <About />
